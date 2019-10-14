b91511f5a81a5e5356b22b533e73fcf28f79f040
srayhit <sambarta1202@outlook.com>
2019-10-14 16:12:33 -0700

    Adding documentation for c1m1

--- stats.h
+++ stats.h
@@ -1,4 +1,4 @@
-/******************************************************************************
+-/******************************************************************************
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
@@ -22,18 +22,82 @@
 #define __STATS_H__
 
 /* Add Your Declarations and Function Comments here */ 
-
+unsigned char find_median(unsigned char *arr, int arrLen);
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief  Given an array of data and a length, returns the median value
  *
  * <Add Extended Description Here>
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param unsigned char *arr input array
+ * @param int arrLen length of the array
  *
- * @return <Add Return Informaiton here>
+ * @return median of arr
+ */
+unsigned char find_mean(unsigned char *arr, int arrLen)
+/**
+ * @brief   Given an array of data and a length, returns the mean
+ *
+ * <Add Extended Description Here>
+ *
+ * @param unsigned char *arr input array
+ * @param int arrLen length of the array
+ *
+ * @return mean of arr
+ */
+unsigned char find_maximum(unsigned char *arr, int arrLen)
+/**
+ * @brief   Given an array of data and a length, returns the maximum
+ *
+ * <Add Extended Description Here>
+ *
+ * @param unsigned char *arr input array
+ * @param int arrLen length of the array
+ *
+ * @return max of arr
+ */
+unsigned char find_minimum(unsigned char *arr, int arrLen)
+/**
+ * @brief  Given an array of data and a length, returns the minimum
+ *
+ * <Add Extended Description Here>
+ *
+ * @param unsigned char *arr input array
+ * @param int arrLen length of the array
+ *
+ * @return min of arr
+ */
+void sort_array(unsigned char *arr, int arrLen)
+/**
+ * @brief   Given an array of data and a length, sorts the array from largest to smallest. (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
+ *
+ * <Add Extended Description Here>
+ *
+ * @param unsigned char *arr input array
+ * @param int arrLen length of the array
+ *
+ * @return 
+ */
+void print_statistics(unsigned char *arr, int arrLen)
+/**
+ * @brief   A function that prints the statistics of an array including minimum, maximum, mean, and median.
+ *
+ * <Add Extended Description Here>
+ *
+ * @param unsigned char *arr input array
+ * @param int arrLen length of the array
+ *
+ * @return 
+ */
+void print_array(unsigned char *arr, int arrLen)
+/**
+ * @brief    Given an array of data and a length, prints the array to the screen
+ *
+ * <Add Extended Description Here>
+ *
+ * @param unsigned char *arr input array
+ * @param int arrLen length of the array
+ *
+ * @return 
  */
 
 
